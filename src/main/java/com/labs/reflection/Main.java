df8dc151b8b1b56fa838901f58bd07b27c4f008f
Claus-Factor <volxin.k2004@gmail.com>
2024-12-23 18:17:53 +0300

    Added Main class for end-point

--- src/main/java/com/labs/reflection/Main.java
+++ src/main/java/com/labs/reflection/Main.java
@@ -1,17 +1,14 @@
 package com.labs.reflection;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.labs.reflection.bean.SomeBean;
+import com.labs.reflection.injector.Injector;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Injector injector = new Injector();
+        injector.loadConfig("path/to/config.properties");
+
+        SomeBean someBean = injector.inject(new SomeBean());
+        someBean.foo(); // Выведет "AC"
     }
 }
